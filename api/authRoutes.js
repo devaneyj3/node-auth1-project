b8d5239528633f16df1b32937c6834ce475482b9
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-06-03 21:38:10 -0400

    hash the password

--- api/authRoutes.js
+++ api/authRoutes.js
@@ -4,10 +4,15 @@ const routes = express.Router();
 
 const db = require('./db_Query')
 
+const bcrypt = require('bcryptjs')
+
 // POST register a user
-routes.post('/register', async(req, res) => {
+routes.post('/register', async (req, res) => {
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hash;
     const addAdmin = await db.PostAdmin(req.body)
-    res.status(201).send(addAdmin)
+    
+    res.status(201).send(req.body)
 })
 
 // //GET login user

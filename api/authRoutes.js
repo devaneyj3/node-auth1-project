ed6f885b06bdac6b3d8eecb8515e0ae43a9b5149
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-06-03 23:34:56 -0400

    use sessions to validate authorization

--- api/authRoutes.js
+++ api/authRoutes.js
@@ -6,19 +6,23 @@ const db = require('./db_Query')
 
 const bcrypt = require('bcryptjs')
 
+const mw = require('./middleware');
+
 // POST register a user
 routes.post('/register', async (req, res) => {
     const hash = bcrypt.hashSync(req.body.password, 10);
     req.body.password = hash;
+
     await db.PostAdmin(req.body)
 
     res.status(201).send(req.body)
 })
 
 //GET login user
-routes.post('/login', async(req, res) => {
-    const { username, password} = req.body;
-
+routes.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    //set the session for the duration of the login
+    req.session.name = username;
     //finds admin with the passed in username
     const user = await db.findAdmin(username)
 
@@ -32,13 +36,36 @@ routes.post('/login', async(req, res) => {
 })
 
 //GET USERS WHEN LOGGED IN
-routes.get('/users', async (req, res) => {
+routes.get('/users', mw.protected, async (req, res) => {
     try {
         const users = await db.getUsers()
-        res.status(200).send(users)
-        
+        if (users) {
+            res.status(200).send(users)
+        } else {
+            res.status(404).json({ message: 'There are no users in the database' })
+        }
+
     } catch (err) {
-        res.status(500).json({message: "There is an error with the server"})
+        res.status(500).json({ message: "There is an error with the server" })
+    }
+})
+//User logs out
+routes.get('/logout', async (req, res) => {
+    try {
+        if (req.session) {
+            req.session.destroy(err => {
+                if (err) {
+                    res.send("Error logging out");
+                } else {
+                    res.send("Good bye")
+                }
+            })
+        } else {
+            res.status(404).json({ message: 'There are no users in the database' })
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: "There is an error with the server" })
     }
 })
 
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const session = require('express-session');
 
 const authRoutes = require('./api/authRoutes');
 const server = express(); 
 
-server.use(express.json());
 
-server.use('/api/auth', authRoutes);
-
-server.get('/', (req, res) => {
-    res.status(200).send('The App is working');
+// configure express-session middleware
+server.use(
+    session({
+        name: 'User', // default is connect.sid
+        secret: 'Never kick Tim',
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+            secure: false, // only set cookies over https. Server will not send back a cookie over http.
+        }, // 1 day in milliseconds
+        httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+        resave: false,
+        saveUninitialized: false,
+    })
+    );
+    
+    server.use(express.json());
+    
+    server.use('/api/auth', authRoutes);
+    
+    server.get('/', (req, res) => {
+        res.status(200).send('The App is working');
 })
 
 const PORT = 5001;
